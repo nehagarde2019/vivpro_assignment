ddccd7cac7b8d30eca561b481524257a05e7fbb9
neha_garde <neha_garde@persistent.com>
2021-09-04 13:16:15 +0530

    get and get by title apis

--- vivpro_assignment/vivpro_assignment/routes.py
+++ vivpro_assignment/vivpro_assignment/routes.py
@@ -1,3 +1,5 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('get_playlist', '/get-playlist')
+    config.add_route('get_song_by_title','get-playlist/{title}')
--- vivpro_assignment/vivpro_assignment/views/default.py
+++ vivpro_assignment/vivpro_assignment/views/default.py
@@ -1,31 +1,57 @@
+from pyramid.renderers import render_to_response
 from pyramid.view import view_config
-from pyramid.response import Response
+import pyramid.httpexceptions as exc
 from sqlalchemy.exc import SQLAlchemyError
 
-from .. import models
+from ..models import Playlist
+
+def result_dict(data_set):
+    if isinstance(data_set, list):
+        data_set = [dict(zip(row.keys(), row)) for row in data_set]
+
+    result_list = []
+    for data in data_set:
+        data_details = {}
+        for column, value in data.items():
+            data_details[column] = value
+        result_list.append(data_details)
+    return result_list
 
 
-@view_config(route_name='home', renderer='vivpro_assignment:templates/mytemplate.jinja2')
-def my_view(request):
+@view_config(route_name='get_playlist')
+def get_playlist(request):
     try:
-        query = request.dbsession.query(models.MyModel)
-        one = query.filter(models.MyModel.name == 'one').one()
-    except SQLAlchemyError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'vivpro_assignement'}
+        result= request.dbsession.query(Playlist).\
+            with_entities(Playlist.id, Playlist.title, Playlist.danceability,
+                        Playlist.energy,Playlist.key,Playlist.loudness,
+                        Playlist.mode,Playlist.acousticness,Playlist.instrumentalness,
+                        Playlist.liveness,Playlist.valence,Playlist.tempo,
+                        Playlist.duration_ms,Playlist.time_signature,Playlist.num_bars,
+                        Playlist.num_sections,Playlist.num_segments,Playlist.class_name).all()
+    except SQLAlchemyError as e:
+        raise exc.HTTPInternalServerError()
+    except Exception as e:
+        return render_to_response(status=500)
+    else:
+        return render_to_response('vivpro_assignment:templates/playlist.jinja2',
+                              {"result":result_dict(result)}, request=request)
 
 
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to initialize your database tables with `alembic`.
-    Check your README.txt for descriptions and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+@view_config(route_name='get_song_by_title')
+def get_song_by_title(request):
+    try:
+        result= request.dbsession.query(Playlist).\
+            with_entities(Playlist.id, Playlist.title, Playlist.danceability,
+                        Playlist.energy,Playlist.key,Playlist.loudness,
+                        Playlist.mode,Playlist.acousticness,Playlist.instrumentalness,
+                        Playlist.liveness,Playlist.valence,Playlist.tempo,
+                        Playlist.duration_ms,Playlist.time_signature,Playlist.num_bars,
+                        Playlist.num_sections,Playlist.num_segments,Playlist.class_name).\
+            filter(Playlist.title==request.matchdict["title"]).all()
+    except SQLAlchemyError as e:
+        raise exc.HTTPInternalServerError()
+    except Exception as e:
+        return render_to_response(status=500)
+    else:
+        return render_to_response('vivpro_assignment:templates/playlist.jinja2',
+                              {"result":result_dict(result)}, request=request)
